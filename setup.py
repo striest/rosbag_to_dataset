0e278f7b71fc4df6441ec05735dba2c6f93c34ed
Mateo Guaman Castro <mguamanc@andrew.cmu.edu>
2022-02-14 11:01:52 -0500

    branch for pip installation

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup, find_packages
 
-d = generate_distutils_setup(
-  packages=['rosbag_to_dataset', 'rosbag_to_dataset.util', 'rosbag_to_dataset.converter', 'rosbag_to_dataset.config_parser', 'rosbag_to_dataset.dtypes'],
-  package_dir={'': 'src'}
+setup(
+    name='rosbag_to_dataset',
+    version='0.0',
+    packages=find_packages(),
+    license='MIT License',
+    long_description=open('README.md').read(),
 )
-
-setup(**d)
