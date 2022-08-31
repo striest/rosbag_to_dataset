93c33323c6ecdf0dc77e1a310b043e1c073d65cc
Parv-Maheshwari <parvmaheshwari2002@gmail.com>
2022-08-31 16:34:07 +0530

    sysid data extraction automated

--- src/rosbag_to_dataset/post_processing/dataloader/datafile_generate_single.py
+++ src/rosbag_to_dataset/post_processing/dataloader/datafile_generate_single.py
@@ -41,21 +41,32 @@ def enumerate_trajs(data_root_dir):
     print('Detected {} trajs'.format(len(trajfolders)))
     return trajfolders
 
-data_root_dir = '/home/parvm/RISS/affix_data/combined_sysid/experiment/rosbag_sync_test/only_traj_2'
-outfile = '/home/parvm/RISS/affix_data/combined_sysid/experiment/rosbag_sync_test/only_traj_2/trainframes.txt'
+def run(root_dir):
+    # root_dir = '/home/parvm/RISS/affix_data/combined_sysid/experiment/only_test'
+    # outfile = '/home/parvm/RISS/affix_data/combined_sysid/experiment/rosbag_sync_test/only_traj_2/trainframes.txt'
 
-f = open(outfile, 'w')
+    single_trajlist = enumerate_trajs(root_dir)
 
-trajlist = enumerate_trajs(data_root_dir)
-for trajdir in trajlist:
-    trajindlist = process_traj(data_root_dir + '/' +trajdir,folderstr = 'image_left_color')
-    for trajinds in trajindlist:
-        f.write(trajdir)
-        f.write(' ')
-        f.write(str(len(trajinds)))
-        f.write('\n')
-        for ind in trajinds:
-            f.write(ind)
-            f.write('\n')
-f.close()
+    for i in range(len(single_trajlist)):
+
+        # data_root_dir = '/home/parvm/RISS/affix_data/combined_sysid/experiment/rosbag_sync_test/only_traj_2'
+        data_root_dir = join(root_dir,single_trajlist[i])
+        outfile= join(data_root_dir,'frames.txt')
+        # outfile = '/home/parvm/RISS/affix_data/combined_sysid/experiment/rosbag_sync_test/only_traj_2/trainframes.txt'
+
+
+        f = open(outfile, 'w')
+
+        trajlist = enumerate_trajs(data_root_dir)
+        for trajdir in trajlist:
+            trajindlist = process_traj(data_root_dir + '/' +trajdir,folderstr = 'image_left_color')
+            for trajinds in trajindlist:
+                f.write(trajdir)
+                f.write(' ')
+                f.write(str(len(trajinds)))
+                f.write('\n')
+                for ind in trajinds:
+                    f.write(ind)
+                    f.write('\n')
+        f.close()
 
