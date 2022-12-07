26699ea7002d60e9d65a030ff505f99c6e81ca92
amigoshan <wenshanw@andrew.cmu.edu>
2022-12-07 16:20:07 -0500

    auto white balance

--- scripts/image_correction.py
+++ scripts/image_correction.py
@@ -5,6 +5,7 @@ import argparse
 from os import listdir
 from os.path import isdir, isfile, join, split
 from rosbag_to_dataset.util.os_util import maybe_mkdir, maybe_rmdir
+import time
 
 # python scripts/data_preview.py --root /project/learningphysics/tartandrive_trajs --bag_list scripts/trajlist.txt --save_to /project/learningphysics/tartandrive_preview
 # python scripts/data_preview.py --root test_output --bag_list scripts/trajlist_local.txt --save_to test_output
@@ -14,7 +15,8 @@ def gammaCorrection(img_original, img_additional=None, roi=None, vis=False):
         roi = [0,img_original.shape[0],0,img_original.shape[1]]
     img = img_original[roi[0]:roi[1], roi[2]:roi[3],:]
 
-    hist, _ = np.histogram(img[:], range=(0,256), bins=256)
+    img_small = cv2.resize(img, (0,0), fx=0.2, fy=0.2) # resize the image to make it faster
+    hist, _ = np.histogram(img_small[:], range=(0,256), bins=256)
     hist_acc = np.cumsum(hist)
     hist_acc = hist_acc / hist_acc[-1]
     lookUpTable = np.clip(hist_acc * 255.0, 0, 255).astype(np.uint8)
@@ -23,7 +25,6 @@ def gammaCorrection(img_original, img_additional=None, roi=None, vis=False):
     if img_additional is not None: # used in the stereo case
         res2 = cv2.LUT(img_additional, lookUpTable)
         res = (res, res2)
-        
     if vis:
         # plt.plot(hist_acc)
         # plt.show()
@@ -34,8 +35,49 @@ def gammaCorrection(img_original, img_additional=None, roi=None, vis=False):
         cv2.waitKey(0)
     return res
 
+def autowhitebalance(img_original, img_additional=None, vis=False):
+    img_small = cv2.resize(img_original, (0,0), fx=0.2, fy=0.2) # resize the image to make it faster
+    # whitepatch = np.percentile(img_small, 95, axis=(0,1))
+    meanvalue = np.mean(img_small, axis=(0,1))
+    # print(whitepatch, meanvalue)
 
-if __name__ == '__main__':
+    res = np.clip(img_original*128.0/meanvalue.reshape(1,1,3), 0, 255).astype(np.uint8)
+    # img_percentile = np.clip(img_small*255.0/whitepatch.reshape(1,1,3), 0, 255).astype(np.uint8)
+    if img_additional is not None: # used in the stereo case
+        res2 = np.clip(img_additional*128.0/meanvalue.reshape(1,1,3), 0, 255).astype(np.uint8)
+        res = (res, res2)
+
+    if vis:
+        cv2.imshow('awb', cv2.hconcat([img_small, img_mean, img_percentile]))
+        cv2.waitKey(0)
+        cv2.imwrite('/home/wenshan/tmp/arl_data/image_correction/'+str(np.random.randint(100))+'.png', cv2.hconcat([img_small, img_mean, img_percentile]))
+    return res
+
+def test_speed():
+    rootfolder = '/home/wenshan/tmp/arl_data/image_correction'
+    imglist = ['001032.png',
+                '001358.png',
+                '001620.png',
+                '001757.png',
+                '001932.png',
+                '002368.png',
+                '002835.png']
+    starttime = time.time()
+
+    for imgfile in imglist:
+        leftimg = cv2.imread(join(rootfolder, 'left', imgfile))
+        rightimg = cv2.imread(join(rootfolder, 'right', imgfile))
+
+        (left_correction, right_correction) = gammaCorrection(leftimg, rightimg, roi=None, vis=False)
+        (left_correction, right_correction) = autowhitebalance(left_correction, right_correction)
+        # import ipdb;ipdb.set_trace()
+        # cv2.imshow("img", left_correction)
+        # cv2.waitKey(0)
+        # cv2.imwrite(join(leftfolder_correction, split(leftimglist[k])[-1]), left_correction)
+        # cv2.imwrite(join(rightfolder_correction, split(rightimglist[k])[-1]), right_correction)
+    print(time.time()-starttime)
+
+def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--root', type=str, required=True, default="", help='Path to the trajectories data')
@@ -68,14 +110,14 @@ if __name__ == '__main__':
         leftfolder = trajdir + '/image_left'
         rightfolder = trajdir + '/image_right' 
 
-        leftfolder_correction = trajdir + '/image_left_correction'
-        rightfolder_correction = trajdir + '/image_right_correction' 
+        leftfolder_correction = trajdir + '/image_left_correction_awb'
+        rightfolder_correction = trajdir + '/image_right_correction_awb' 
         maybe_mkdir(leftfolder_correction)
         maybe_mkdir(rightfolder_correction)
 
-        outvidfile = join(args.save_to, outfolder + '_color_correction.mp4')
+        outvidfile = join(args.save_to, outfolder + '_color_correction_awb.mp4')
         fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-        fout=cv2.VideoWriter(outvidfile, fourcc, 10.0, (1440, 540))
+        fout=cv2.VideoWriter(outvidfile, fourcc, 10.0, (1080, 270))
 
         leftimglist = listdir(leftfolder)
         leftimglist = [leftfolder + '/' + img for img in leftimglist if img.endswith('.png')]
@@ -92,11 +134,15 @@ if __name__ == '__main__':
             rightimg = cv2.imread(rightimglist[k])
 
             (left_correction, right_correction) = gammaCorrection(leftimg, rightimg, roi=None, vis=False)
+            (left_correction_awb, right_correction_awb) = autowhitebalance(left_correction, right_correction)
             # import ipdb;ipdb.set_trace()
-            cv2.imwrite(join(leftfolder_correction, split(leftimglist[k])[-1]), left_correction)
-            cv2.imwrite(join(rightfolder_correction, split(rightimglist[k])[-1]), right_correction)
+            cv2.imwrite(join(leftfolder_correction, split(leftimglist[k])[-1]), left_correction_awb)
+            cv2.imwrite(join(rightfolder_correction, split(rightimglist[k])[-1]), right_correction_awb)
 
-            vis = cv2.resize(cv2.hconcat([leftimg, left_correction]), (0,0), fx=0.5, fy=0.5)
+            vis = cv2.resize(cv2.hconcat([leftimg, left_correction, left_correction_awb]), (0,0), fx=0.25, fy=0.25)
             fout.write(vis)
 
-        fout.release()
+        fout.release()    
+
+if __name__ == '__main__':
+    main()
