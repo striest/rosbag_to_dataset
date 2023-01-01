d5a79035b0facf5e806577501b7b95125b498001
Parv-Maheshwari <parvmaheshwari2002@gmail.com>
2023-01-02 00:49:17 +0530

    steer interpolation + original preprocess script

---
+++ scripts/interpolate_steer.py
@@ -0,0 +1,57 @@
+import argparse
+from os import listdir
+from os.path import join,isdir
+import numpy as np
+import scipy.interpolate
+import matplotlib.pyplot as plt
+from rosbag_to_dataset.util.os_util import maybe_mkdir
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--source_fp', type=str, required=True, help='Path to the source directory')
+    parser.add_argument('--save_fp', type=str, required=True, help='Path to the destination trajectory')
+
+    args = parser.parse_args()
+
+    source_fp = args.source_fp
+    save_fp = args.save_fp
+
+    traj_folders = [x for x in listdir(source_fp) if isdir(join(source_fp,x))]
+    for traj_name in traj_folders:
+        source_traj_fp = join(source_fp,traj_name)
+        dest_traj_fp = join(save_fp,traj_name)
+        source_delta = np.load(join(source_traj_fp,'delta','float.npy'))
+        delta_timestamps = np.loadtxt(join(source_traj_fp,'delta','timestamps.txt'))
+
+        dest_timestamps = np.loadtxt(join(dest_traj_fp,'odom','timestamps.txt'))
+        interpolated_delta = scipy.interpolate.interp1d(delta_timestamps, source_delta)
+
+        missed_first_timestamps = dest_timestamps[dest_timestamps < delta_timestamps[0]]
+        missed_last_timestamps = dest_timestamps[dest_timestamps > delta_timestamps[-1]]
+
+        missed_first_delta = np.full((len(missed_first_timestamps)), source_delta[0])
+        missed_last_delta = np.full((len(missed_last_timestamps)), source_delta[-1])
+
+        overlap_idx = np.logical_and(dest_timestamps >= delta_timestamps[0], dest_timestamps <= delta_timestamps[-1])
+
+        final_delta = np.concatenate((missed_first_delta,interpolated_delta(dest_timestamps[overlap_idx]),missed_last_delta))
+        maybe_mkdir(join(dest_traj_fp,'delta'))
+        np.save(join(dest_traj_fp,'delta','float.npy'),final_delta)
+        np.savetxt(join(dest_traj_fp,'delta','timestamps.txt'),dest_timestamps)
+
+        # plt.plot(delta_timestamps,source_delta,'r-',label='Original Steering')
+        # plt.plot(dest_timestamps,final_delta,'b.',label='Interploated Steering')
+        # plt.legend()
+        # plt.show()
+
+
+
+
+
+
+
+
+
+
---
+++ scripts/og_preprocess_dataset.py
@@ -0,0 +1,85 @@
+from os import listdir
+
+# rootfolder = '/project/learningphysics/dataset'
+# subfolders = ['20210812','20210812_interventions','20210826_bags','20210828','20210902','20210903','20210910_bags','gascola_laps']
+
+# outtxt = open('trajlist.txt','w')
+# for k, subfolder in enumerate(subfolders):
+#     subdir = rootfolder + '/' + subfolder
+#     bagfiles = listdir(subdir)
+#     bagfiles = [bb for bb in bagfiles if bb.endswith('.bag')]
+#     bagfiles.sort()
+#     print('find {} bagfiles in {}'.format(len(bagfiles), subdir))
+
+#     for bag in bagfiles:
+#         outtxt.write(subdir + '/' + bag)
+#         outtxt.write(' ')
+#         if k<2:
+#             outtxt.write(subfolder+'_'+bag.split('.bag')[0])
+#         else:
+#             outtxt.write(bag.split('.bag')[0])
+#         outtxt.write('\n')
+# outtxt.close()
+
+
+# rootfolder = '/project/learningphysics/2022-06-13'
+# subfolders = [
+#     'exp2_rough_rider_down_run1',
+#     'exp2_turnpike_3ms_1',
+#     'exp3_mppi_baseline_turnpike_pole_1',
+#     'exp3_mppi_baseline_turnpike_pole_2',
+#     'exp3_mppi_baseline_turnpike_pole_3',
+#     'exp3_mppi_baseline_warehouse_1',
+#     'exp3_mppi_baseline_warehouse_2',
+#     'exp3_mppi_baseline_warehouse_3',
+#     'exp3_mppi_baseline_warehouse_4',
+#     'exp3_mppi_baseline_warehouse_5',
+#     'exp3_mppi_both_warehouse_1',
+#     'exp3_mppi_both_warehouse_2',
+#     'exp3_mppi_both_warehouse_3',
+#     'exp3_mppi_both_warehouse_4',
+#     'exp3_mppi_both_warehouse_5',
+#     'exp3_mppi_fused_turnpike_pole_2',
+#     'exp3_mppi_fused_turnpike_pole_3',
+#     'exp3_mppi_learned_turnpike_pole_1',
+#     'mppi_test_run',
+# ]
+
+'''
+rootfolder = '/project/learningphysics/corl_sara/2022-06-19'
+subfolders = [
+    'exp1_warehouse_baseline_1',
+    'exp1_warehouse_baseline_2',
+    'exp1_warehouse_baseline_3',
+    'exp1_warehouse_both_warehouse_1',
+    'exp1_warehouse_both_warehouse_2',
+    'exp2_figure8_both',
+    'exp2_figure8_both_part2',
+]
+'''
+
+rootfolder = '/home/parvm/RISS/data'
+subfolders = [
+    'test_single_tartan',
+    # 'exp1_warehouse_baseline_2',
+    # 'exp1_warehouse_baseline_3',
+    # 'exp1_warehouse_both_warehouse_1',
+    # 'exp1_warehouse_both_warehouse_2',
+    # 'exp2_figure8_both',
+    # 'exp2_figure8_both_part2',
+]
+outtxt = open('trajlist_tartan_single.txt','w')
+for k, subfolder in enumerate(subfolders):
+    subdir = rootfolder + '/' + subfolder
+    bagfiles = listdir(subdir)
+    bagfiles = [bb for bb in bagfiles if bb.endswith('.bag')]
+    bagfiles.sort()
+    print('find {} bagfiles in {}'.format(len(bagfiles), subdir))
+
+    for k,bag in enumerate(bagfiles):
+        outtxt.write(subdir + '/' + bag)
+        outtxt.write(' ')
+        # outtxt.write(subfolder+'_'+ str(k))
+        outtxt.write(bag[:-4])
+        outtxt.write('\n')
+outtxt.close()
