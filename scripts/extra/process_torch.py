3f4374bbca8586637b380faa186f9460a88eb700
Parv-Maheshwari <parvmaheshwari2002@gmail.com>
2023-06-04 21:46:36 -0400

    process torch sync

--- scripts/extra/process_torch.py
+++ scripts/extra/process_torch.py
@@ -5,12 +5,18 @@ from tqdm import tqdm
 from copy import deepcopy
 import numpy as np
 import scipy
-root_fp ='/project/learningphysics/parvm/torch_2022/remaining_vel_split/vel/less_than_3.0'
+root_fp ='/project/learningphysics/parvm/torch_2021/vel_split/vel/between_3.0_5.0'
 traj_names = listdir(root_fp)
 
 for traj_name in tqdm(traj_names):
     traj_fp = join(root_fp,traj_name)
     traj = torch.load(traj_fp)
+    if traj['next_observation']['new_state'].shape[-1] == 16:
+        continue
 
-    traj['terminal'][-1] = True
+    obs_steer = traj['observation']['steer_angle'] * (30./415.) * (-torch.pi/180.)
+    traj['observation']['new_state'] = torch.cat([traj['observation']['new_state'],obs_steer],dim=-1)
+
+    next_obs_steer = traj['next_observation']['steer_angle'] * (30./415.) * (-torch.pi/180.)
+    traj['next_observation']['new_state'] = torch.cat([traj['next_observation']['new_state'],next_obs_steer],dim=-1)
     torch.save(traj,traj_fp)
