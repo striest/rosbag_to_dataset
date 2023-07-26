17c66474d17d6e4bf28cbe9ca284929fe128e80d
Parv Archana Maheshwari <54958733+Parv-Maheshwari@users.noreply.github.com>
2023-07-26 18:37:20 -0400

    Update download_models.sh

--- src/rosbag_to_dataset/post_processing/tartanvo/download_models.sh
+++ src/rosbag_to_dataset/post_processing/tartanvo/download_models.sh
@@ -2,3 +2,4 @@ mkdir models
 wget https://cmu.box.com/shared/static/dz55kua11vdxg34jwp0yjc4rioopnbvm.pkl -O ./models/5_5_4_stereo_30000.pkl
 wget https://cmu.box.com/shared/static/njbsuqkd92itmg1o0r3npvkcn4n3jmxt.pkl -O ./models/43_6_2_vonet_30000.pkl
 wget https://cmu.box.com/shared/static/mvqsd1w6qju60p7w6mrfahh7zr1dnsmi.pkl -O ./models/43_6_2_vonet_30000_wo_pwc.pkl
+wget https://cmu.box.com/shared/static/xrmr1hwsex0wesa5x629k9bsxs321it8.pkl -O ./models/6_3_2_stereo_60000.pkl
