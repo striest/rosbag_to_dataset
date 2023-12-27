5bad6fdec22831e81a78085c6d48949a4bbecde8
Wenshan Wang <wenshanw@andrew.cmu.edu>
2023-12-27 01:57:13 -0500

    add float32multiarray

---
+++ src/rosbag_to_dataset/dtypes/float32multiarray.py
@@ -0,0 +1,44 @@
+import rospy
+import numpy as np
+
+from std_msgs.msg import Float32MultiArray
+
+from rosbag_to_dataset.dtypes.base import Dtype
+
+class Float32MultiArrayConvert(Dtype):
+    """
+    Convert an odometry message into a 13d vec.
+    """
+    def __init__(self):
+        pass
+
+    def N(self):
+        return 1
+
+    def rosmsg_type(self):
+        return Float32MultiArray
+
+    def ros_to_numpy(self, msg):
+#        assert isinstance(msg, self.rosmsg_type()), "Got {}, expected {}".format(type(msg), self.rosmsg_type())
+        return np.array(msg.data)
+
+    def save_file_one_msg(self, data, filename):
+        """
+        Save the data to hard drive.
+        This function should be implemented where the data is stored frame by frame like image or point cloud
+        """
+        return self.ros_to_numpy(data)
+
+    def save_file(self, data, filename):
+        """
+        Save the data to hard drive.
+        This function should be implemented where the data of the whole trajectory is stored in one file, like imu, odometry, etc.
+        """
+        np.save(filename+'/float.npy', data)
+
+if __name__ == "__main__":
+    c = Float32MultiArrayConvert()
+    msg = Float32MultiArray()
+    msg.data=[1,2,3]
+
+    print(c.ros_to_numpy(msg))
